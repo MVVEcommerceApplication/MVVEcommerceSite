86fdac5ba5885d5416ba0b13d06d5d997bdf0963
Ousman <ordiallo@gmail.com>
2020-01-20 18:49:52 -0500

    updated React Boilerplate

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-  constructor(props) {
+  constructor() {
     super();
   }
 
   render() {
     return (
       <div>
-        <h1> Hello React stuff rendering</h1>
+        <h1> Hello React App!!</h1>
       </div>
     );
   }
