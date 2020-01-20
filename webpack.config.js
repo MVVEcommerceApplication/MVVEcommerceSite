01bfa9197a663960b70ae759a190176626ba5a1f
Benjamin Mizel <benjaminmizel@Shiras-MacBook-Pro.local>
2020-01-20 16:14:33 -0500

    initial setup

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './client/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  devServer: {
+    publicPath: '/build/',
+    port: 8080,
+    proxy: {
+      '/': 'http://localhost:3000',
+    },
+    contentBase: './client',
+  },
+};
