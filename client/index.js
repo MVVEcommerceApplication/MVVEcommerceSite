21a5378bb2a6377d6b76d1954e585e4e077c48c8
Tim <tim.atapagra@gmail.com>
2020-01-20 18:38:01 -0500

    Basic react app spun

---
+++ client/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
+import App from './components/App.jsx';
+
+render(
+  <App />,
+  document.getElementById('root'),
+);
