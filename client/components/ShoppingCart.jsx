25e4ec1b56bae76ad8cba648d2bd5265de06ae99
Ousman <ordiallo@gmail.com>
2020-01-20 22:02:58 -0500

    basic react router functional

---
+++ client/components/ShoppingCart.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+// eslint-disable-next-line arrow-body-style
+const ShoppingCart = () => {
+//   const x = 0;
+
+  return (
+    <div>
+      <h1>Shopping Cart Component</h1>
+    </div>
+  );
+};
+
+export default ShoppingCart;
