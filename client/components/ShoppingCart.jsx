bdd437fee7c72cdbae24637e0d7c69319ab385af
Ousman <ordiallo@gmail.com>
2020-01-21 19:04:52 -0500

    shopping cart moved to Class compnent

--- client/components/ShoppingCart.jsx
+++ client/components/ShoppingCart.jsx
@@ -1,35 +1,57 @@
-import React, { Component, useState } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import ShoppingCartProduct from './product/ShoppingCartProduct.jsx';
 
-// eslint-disable-next-line arrow-body-style
-const ShoppingCart = () => {
-//   const x = 0;
-  const [currentProducts, updateCurrent] = useState([{
-    itemName: 'Tote Bag',
-    description: 'Designed for anything from grocery shopping to running every day errands, totes are simply a staple of the modern world.',
-    quantity: 3,
-    price: 14.77,
-  }]);
 
-  return (
-    <div>
-      <h1>Shopping Cart</h1>
+class ShoppingCart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      itemName: 'Tote Bag',
+      description: 'Designed for anything from grocery shopping to running every day errands, totes are simply a staple of the modern world.',
+      quantity: 0,
+      price: 14.77,
+
+    };
+    this.incCount = this.incCount.bind(this);
+  }
+
+  incCount() {
+    console.log('COUNTER BUTTON CLICKED!');
+    this.setState({
+      quantity: this.state.quantity + 1,
+    });
+  }
+
+  render() {
+    return (
       <div>
-        <ShoppingCartProduct
-          itemName={currentProducts[0].itemName}
-          description={currentProducts[0].description}
-          quantity={currentProducts[0].quantity}
-          price={currentProducts[0].price}
-        />
+        <h1>Shopping Cart</h1>
+        <div>
+          <ShoppingCartProduct
+            itemName={this.state.itemName}
+            description={this.state.description}
+            quantity={this.state.quantity}
+            price={this.state.price}
+          />
+        </div>
+        <div>
+          <h5>subtotal: </h5>
+        </div>
+        <div>
+          <button type="button">
+            Proceed to Checkout
+          </button>
+        </div>
+        <button
+          type="button"
+          onClick={() => this.incCount()}
+        >
+          {`Counter ${this.state.quantity}`}
+        </button>
       </div>
-      <div>
-        <h5>subtotal: </h5>
-      </div>
-      <div>
-        <button>Proceed to Checkout</button>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
 
 export default ShoppingCart;
