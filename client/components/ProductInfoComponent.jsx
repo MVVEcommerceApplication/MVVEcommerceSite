c93cd2b4ee91128a4bea15961b37c195e9e7ec50
Tim <tim.atapagra@gmail.com>
2020-01-22 12:21:05 -0500

    New component for each product

---
+++ client/components/ProductInfoComponent.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+
+const ProductInfo = (props) => {
+  const x = 0;
+  // console.warn('This is from Product info component ', props.image);
+  return (
+    <div className="container">
+
+      <div className="productBox">
+        <img src={props.image} />
+        <p>
+          {props.name}
+          {' '}
+          {' '}
+        </p>
+        <p>
+          {props.description}
+          {' '}
+        </p>
+        <p>
+          {props.price}
+          {' '}
+        </p>
+        <p><button type="button">Add to Cart</button></p>
+      </div>
+    </div>
+
+
+  );
+};
+
+
+export default ProductInfo;
