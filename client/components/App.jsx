630cb5098db1fe84e4a85be8dcf411808bd7bf77
Ousman <ordiallo@gmail.com>
2020-01-22 15:01:57 -0500

    center divs

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/button-has-type */
 /* eslint-disable react/prefer-stateless-function */
 import React, { Component } from 'react';
-import CheckoutContainer from './checkout/CheckoutContainer.jsx';
 import { render } from 'react-dom';
 import {
   BrowserRouter as Router,
@@ -10,6 +9,7 @@ import {
   Link,
 } from 'react-router-dom';
 import { connect } from 'react-redux';
+import CheckoutContainer from './checkout/CheckoutContainer.jsx';
 import ShoppingCart from './ShoppingCart.jsx';
 import NavDropDown from './NavDropDown.jsx';
 import Home from './Home.jsx';
@@ -18,8 +18,8 @@ import style from '../assets/styling/masterStyle.css';
 import reducers from '../reducers/index.js';
 import storeReducer from '../reducers/storeReducer.js';
 import * as actions from '../actions/actions';
-import Signup from '../components/Authentication/Signup.jsx';
-import Login from '../components/Authentication/Login.jsx';
+import Signup from './Authentication/Signup.jsx';
+import Login from './Authentication/Login.jsx';
 
 const mapStateToProps = (state) => ({
   // add pertinent state here
@@ -58,14 +58,14 @@ class App extends Component {
             <div>
               <h2>MVV Fashion Shop</h2>
             </div>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/cart" component={ShoppingCart} />
+              <Route exact path="/checkout" component={CheckoutContainer} />
+            </Switch>
           </div>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/signup" component={Signup} />
-            <Route exact path="/cart" component={ShoppingCart} />
-            <Route exact path="/checkout" component={CheckoutContainer} />
-          </Switch>
         </div>
       </Router>
     );
