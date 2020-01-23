405534c4eaf32d40598face4159b07e794b6b1eb
Ousman <ordiallo@gmail.com>
2020-01-22 20:13:46 -0500

    updated server with product middleware

--- server/server.js
+++ server/server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const path = require('path');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 const port = 3000;
 
 const userControllers = require('./controllers/userControllers');
+const productsController = require('./controllers/productsControllers');
 
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
 
 app.post('/signup', userControllers.createUser, (req, res, next) => {
   // after successful signup will redirect to main page or to login
   res.status(200).send('signup successful');
 });
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, '../index.html')));
+// read products from database endpoints
+app.get('/products', productsController.readProducts, (req, res) => {
+  res.status(200).json(res.locals.products);
+});
+
 
 // fixes react router re-render issues
 app.get('/*', (req, res) => {
